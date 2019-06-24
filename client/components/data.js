f49cfbf8d99f5a234fdb0a90f85d9d8a3c1e1aef
driver620 <drivera453@hotmail.com>
2019-06-24 02:21:29 -0400

    Chore: Component made for data. Data Navbar option appears only when logged in

---
+++ client/components/data.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Data extends Component {
+  render() {
+    console.log('This is the data component')
+    return (
+      <div>
+        <h1> Testing Data</h1>
+      </div>
+    )
+  }
+}
+
+export default Data
